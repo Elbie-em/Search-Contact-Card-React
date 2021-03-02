46658837b02fb5da4f87b28399ddc34680abff17
Elbie-em <emoonga1993@gmail.com>
2021-03-02 23:03:21 +0200

    Get example data from module

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SearchContactCard from '../components/SearchContactCard';
+import getExampleData from '../staticData/exampleData';
 import '../styles/App.css';
 
 const App = () => {
+  const {fullName,profileImageUrl,trust} = getExampleData();
   return (
     <>
     <div>
-      <SearchContactCard />
+      <SearchContactCard name={fullName} imgUrl={profileImageUrl} score={trust}/>
     </div>
     </>
   );
