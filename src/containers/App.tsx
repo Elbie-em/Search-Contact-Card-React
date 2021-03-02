e51727a08826df68b1942ebda4ea63c3ce607868
Elbie-em <emoonga1993@gmail.com>
2021-03-02 18:05:13 +0200

    Render Search Contact card to app component

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import SearchContactCard from '../components/SearchContactCard';
 import '../styles/App.css';
 
 const App = () => {
   return (
     <div className="App">
-      
+      <SearchContactCard />
     </div>
   );
 }
