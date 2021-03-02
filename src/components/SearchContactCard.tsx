7172881de194c3db55a7e239ff168630ede38ed1
Elbie-em <emoonga1993@gmail.com>
2021-03-02 20:24:18 +0200

    Add props to gradient progress component

--- src/components/SearchContactCard.tsx
+++ src/components/SearchContactCard.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react'
 import useStyles from '../styles/styles'
 import GradientProgress from './GradientProgress';
@@ -9,7 +10,8 @@ const SearchContactCard = () => {
       <div className={classes.profileCard}>
         <div className={classes.group622}>
           <div className={classes.scoreChart}>
-            <GradientProgress />
+            <GradientProgress value={75} size={80}/>
+            <Avatar className={classes.profileImage} alt="randomUser" src="https://randomuser.me/api/portraits/women/56.jpg"/>
           </div>
         </div>
       </div>
