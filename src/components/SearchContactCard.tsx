ffeadad5f549106b93d7722ce8b47e3520a2c029
Elbie-em <emoonga1993@gmail.com>
2021-03-02 18:04:18 +0200

    Start build on SearchContactCard containers

--- src/components/SearchContactCard.tsx
+++ src/components/SearchContactCard.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import useStyles from '../styles/styles'
 
 const SearchContactCard = () => {
+  const classes = useStyles();
   return (
-    <div>
-      
+    <div className={classes.scContainer}>
+      <div className={classes.profileCard}>
+        <div className={classes.group622}>
+
+        </div>
+      </div>
+      <div className={classes.rect174}>
+
+      </div>
+      <div className={classes.rect175}>
+
+      </div>
     </div>
   )
 }
