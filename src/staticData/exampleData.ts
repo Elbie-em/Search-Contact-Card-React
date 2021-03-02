5f2895ee3c08bb16197101af343485a20f044c2d
Elbie-em <emoonga1993@gmail.com>
2021-03-02 23:02:58 +0200

    Add example data

---
+++ src/staticData/exampleData.ts
@@ -0,0 +1,9 @@
+
+const getExampleData = () => ({
+  userId: 1234,
+  fullName: 'Gina Snelly',
+  profileImageUrl: 'https://randomuser.me/api/portraits/women/58.jpg',
+  trust: 75
+})
+
+export default getExampleData;
